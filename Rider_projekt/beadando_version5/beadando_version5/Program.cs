7ed38b3bef572e3bb5c381bee8de56d1c936da31
Teker Balázs <lordfoxturtle13@gmail.com>
2025-12-06 13:41:57 +0100

    ez meg egy teszt

--- Rider_projekt/beadando_version5/beadando_version5/Program.cs
+++ Rider_projekt/beadando_version5/beadando_version5/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("\n\nEz most a TBALAZS BRANCH!!\n\n");
+
+        // ez meg előtte van
+        Console.WriteLine("kacsa-liba");
+        
+        
+        //még több komment
+        
+        
+        
+        //még több
     }
 }
